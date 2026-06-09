f2dd861d86f51143b23f46c5ff3c231abb9b4226
Bill Ulises Hernandez Hernandez <bill.hernandez@mh.gob.sv>
2026-06-09 10:19:09 -0600

    primer commit

---
+++ seguimientos/src/main/java/com/mh/seguimientos/SeguimientosApplication.java
@@ -0,0 +1,13 @@
+package com.mh.seguimientos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeguimientosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeguimientosApplication.class, args);
+	}
+
+}
---
+++ seguimientos/src/test/java/com/mh/seguimientos/SeguimientosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mh.seguimientos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeguimientosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
